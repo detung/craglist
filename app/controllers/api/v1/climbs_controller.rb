79eb4286b48c96e83c3a38f22c522564be2d6240
Dennis Tung <detung@gmail.com>
2018-02-10 14:13:28 -0500

    temp workaround of user

--- app/controllers/api/v1/climbs_controller.rb
+++ app/controllers/api/v1/climbs_controller.rb
@@ -1,37 +1,40 @@
 class Api::V1::ClimbsController < ApiController
 
   def create
-    if current_user.nil?
-      redirect_to new_user_session_path
-    else
-      user = current_user
+    # if current_user.nil?
+    #   redirect_to new_user_session_path
+    # else
+      # user = current_user
+      user = User.first
       climb = Climb.new(climb_params)
 
       if climb.save!
         user.climbs << climb
         render json: user.climbs_to_do
       end
-    end
+    # end
   end
 
   def todos
-    if current_user.nil?
-      redirect_to new_user_session_path
-    else
-      user = current_user
+    # if current_user.nil?
+    #   redirect_to new_user_session_path
+    # else
+      # user = current_user
+      user = User.first
       to_do_list = user.climbs_to_do
       render json: to_do_list
-    end
+    # end
   end
 
   def ticks
-    if current_user.nil?
-      redirect_to new_user_session_path
-    else
-      user = current_user
+    # if current_user.nil?
+    #   redirect_to new_user_session_path
+    # else
+      # user = current_user
+      user = User.first
       tick_list = user.climbs_completed.reverse
       render json: tick_list
-    end
+    # end
   end
 
   private
