1d3658a79ad163545a0c29c7628c6a6d0f19e69e
Dennis Tung <detung@gmail.com>
2018-02-13 18:39:30 -0500

    add more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ if Rails.env.development?
     user.password = "password"
   end
 
-  climb1 = Climb.find_or_create_by!(
+  clusterphobia = Climb.find_or_create_by!(
     name: "Clusterphobia",
     location: "Rumney, NH",
     grade: "5.10d",
@@ -18,7 +18,7 @@ if Rails.env.development?
     pitches: 1,
     description: "A fun slightly steep climb"
   )
-  climb2 = Climb.find_or_create_by!(
+  orangahang = Climb.find_or_create_by!(
     name: "Orangahang",
     location: "Rumney, NH",
     grade: "5.12a",
@@ -26,7 +26,7 @@ if Rails.env.development?
     pitches: 1,
     description: "Steep powerful climbing on jugs"
   )
-  climb3 = Climb.find_or_create_by!(
+  peer_pressure = Climb.find_or_create_by!(
     name: "Peer Pressure",
     location: "Rumney, NH",
     grade: "5.10d",
@@ -34,7 +34,7 @@ if Rails.env.development?
     pitches: 1,
     description: "Big rest in the middle"
   )
-  climb4 = Climb.find_or_create_by!(
+  underdog = Climb.find_or_create_by!(
     name: "Underdog",
     location: "Rumney, NH",
     grade: "5.10a",
@@ -42,16 +42,78 @@ if Rails.env.development?
     pitches: 1,
     description: "Fun. Unique hold"
   )
+  metamorphosis = Climb.find_or_create_by!(
+    name: "Metamorphosis",
+    location: "Rumney, NH",
+    grade: "5.8",
+    discipline: "Sport",
+    pitches: 1
+  )
+  masterpiece = Climb.find_or_create_by!(
+    name: "Masterpiece",
+    location: "Rumney, NH",
+    grade: "5.10a",
+    discipline: "Sport",
+    pitches: 1
+  )
+  glory_jeans = Climb.find_or_create_by!(
+    name: "Glory Jeans",
+    location: "Rumney, NH",
+    grade: "5.7",
+    discipline: "Sport",
+    pitches: 1
+  )
+  epinephrine = Climb.find_or_create_by!(
+    name: "Epinephrine",
+    location: "Red Rock, NV",
+    grade: "5.9",
+    discipline: "Trad",
+    pitches: 13
+  )
+  thin_air = Climb.find_or_create_by!(
+    name: "Thin Air",
+    location: "Cathedral Ledge, NH",
+    grade: "5.6",
+    discipline: "Trad",
+    pitches: 4
+  )
+  snake_dike = Climb.find_or_create_by!(
+    name: "Snake Dike",
+    location: "Yosemite, CA",
+    grade: "5.7",
+    discipline: "Trad",
+    pitches: 8
+  )
+  whitney_gilman = Climb.find_or_create_by!(
+    name: "Whitney Gilman Ridge",
+    location: "Cannon Cliff, NH",
+    grade: "5.7",
+    discipline: "Trad",
+    pitches: 5
+  )
 
   user.climbs = Climb.all
 
-  Comment.find_or_create_by!(body: "Want to lead this one", user: user, climb: climb1)
-  Comment.find_or_create_by!(body: "Maybe my first 5.12a?", user: user, climb: climb2)
-  Comment.find_or_create_by!(body: "Proud of this lead", user: user, climb: climb3)
-  Comment.find_or_create_by!(body: "First 5.10a onsight", user: user, climb: climb4)
-  
-  tick1 = ToDo.find_by(user: user, climb: Climb.third)
-  tick1.completed!
-  tick2 = ToDo.find_by(user: user, climb: Climb.last)
-  tick2.completed!
+  Comment.find_or_create_by!(body: "Want to lead this one", user: user, climb: clusterphobia)
+  Comment.find_or_create_by!(body: "Maybe my first 5.12a?", user: user, climb: orangahang)
+  Comment.find_or_create_by!(body: "Proud of this lead", user: user, climb: peer_pressure)
+  Comment.find_or_create_by!(body: "First 5.10a onsight", user: user, climb: underdog)
+  Comment.find_or_create_by!(body: "Really fun climb on juggy holds", user: user, climb: metamorphosis)
+  Comment.find_or_create_by!(body: "Steep beginning and interesting stem finish", user: user, climb: masterpiece)
+  Comment.find_or_create_by!(body: "First lead at Rumney", user: user, climb: glory_jeans)
+  Comment.find_or_create_by!(body: "Amazing big wall", user: user, climb: epinephrine)
+  Comment.find_or_create_by!(body: "Maybe first trad with Fletcher", user: user, climb: thin_air)
+  Comment.find_or_create_by!(body: "Half Dome is epic", user: user, climb: snake_dike)
+  Comment.find_or_create_by!(body: "Looks really cool", user: user, climb: whitney_gilman)
+
+  climbs_ticked = []
+  climbs_ticked << ToDo.find_by(user: user, climb: peer_pressure)
+  climbs_ticked << ToDo.find_by(user: user, climb: underdog)
+  climbs_ticked << ToDo.find_by(user: user, climb: metamorphosis)
+  climbs_ticked << ToDo.find_by(user: user, climb: masterpiece)
+  climbs_ticked << ToDo.find_by(user: user, climb: glory_jeans)
+  # tick1 = ToDo.find_by(user: user, climb: peer_pressure)
+  # tick1.completed!
+  # tick2 = ToDo.find_by(user: user, climb: underdog)
+  # tick2.completed!
 end
